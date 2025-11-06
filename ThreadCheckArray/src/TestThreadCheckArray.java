17498527f4da5f465c5005fedc7141726d38a2f3
d-hessen <burgzing@gmail.com>
2025-11-06 14:41:17 +0200

    Update

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -11,7 +11,7 @@ public class TestThreadCheckArray {
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
-				array.add(input.nextInt());
+				array.add(index, input.nextInt());
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
